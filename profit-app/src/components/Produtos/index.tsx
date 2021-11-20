8032ef59465d5c2188c44c73d43c32b097506e21
kelvyntelles <kelvyn.telles@gmail.com>
2021-11-20 14:55:25 -0300

    criando lista de produtos

--- profit-app/src/components/Produtos/index.tsx
+++ profit-app/src/components/Produtos/index.tsx
@@ -1,9 +1,54 @@
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
+import tenis1 from '../../assets/tenis1.jpg';
 
 export function Produtos() {
     return (
         <Container>
-            <h3>Produtos</h3>
+            <Content>
+                <h3>Produtos</h3>
+                <div className="listaProdutos">
+                    <div className="item">
+                        <img className="imagemProduto" src={tenis1} alt="tenis de corrida" />
+                        <div>
+                            <p>TÊNIS COURO PUMA</p>
+                            <p><s>de R$ 399,00</s></p>
+                            <h4>por R$ 299,00</h4>
+                            <p>ou em 3x de R$ 99,67</p>
+                            <button type="button">Comprar</button>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <img className="imagemProduto" src={tenis1} alt="tenis de corrida" />
+                        <div>
+                            <p>TÊNIS COURO PUMA</p>
+                            <p><s>de R$ 399,00</s></p>
+                            <h4>por R$ 299,00</h4>
+                            <p>ou em 3x de R$ 99,67</p>
+                            <button type="button">Comprar</button>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <img className="imagemProduto" src={tenis1} alt="tenis de corrida" />
+                        <div>
+                            <p>TÊNIS COURO PUMA</p>
+                            <p><s>de R$ 399,00</s></p>
+                            <h4>por R$ 299,00</h4>
+                            <p>ou em 3x de R$ 99,67</p>
+                            <button type="button">Comprar</button>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <img className="imagemProduto" src={tenis1} alt="tenis de corrida" />
+                        <div>
+                            <p>TÊNIS COURO PUMA</p>
+                            <p><s>de R$ 399,00</s></p>
+                            <h4>por R$ 299,00</h4>
+                            <p>ou em 3x de R$ 99,67</p>
+                            <button type="button">Comprar</button>
+                        </div>
+                    </div>
+                </div>
+            </Content>
         </Container>
     )
 }
--- profit-app/src/components/Produtos/styles.ts
+++ profit-app/src/components/Produtos/styles.ts
@@ -2,6 +2,56 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     background: #FFFFFF;
-    height: 350px;
+    min-height: 400px;
+    padding-top: 2rem;
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+
+    .listaProdutos {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        padding-top: 1rem;
+        padding-bottom: 2rem;
+
+        .item {
+            background: #F2F2F2;
+            text-align: center;
+            margin-right: 1rem;
+            border: 1px solid #F2F2F2;
+            box-sizing: border-box;
+            border-radius: 0.25rem;
+            padding-bottom: 1rem;
+            width: 200px;
+
+            .imagemProduto {
+                width: 120px;
+                padding-top: 1rem;
+                padding-bottom: 1rem;
+            }
+
+            h4 {
+                color: #2EC4B6;
+            }
+
+            button {
+                background: #2EC4B6;
+                color: #FFFFFF;
+                font-weight: 500;
+                border: 0;
+                border-radius: 0.25rem;
+                width: 80%;
+                padding: 0.5rem 0 0.5rem 0;
+
+                transition: filter 0.2s;
+
+                &:hover {
+                    filter: brightness(0.9);
+                }
+            }
+        }
+    }
 `
 
